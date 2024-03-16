1bb0cae2ce3739e3f1b61294d70a3eed1901c6b7
d1mitrii <dyusmirnov@stud.etu.ru>
2024-03-17 00:20:13 +0300

    docs: add some comments to tables manager

--- backend/app/tables/tables_manager.py
+++ backend/app/tables/tables_manager.py
@@ -4,6 +4,9 @@ from .gsheets.table import Table
 from typing import Dict
 
 class TablesManager():
+    """
+    Tables Manager help's to control tables
+    """
     __tables: Dict[str, asyncio.Task] = {}
     __event_loop = None
     def __init__(self) -> None:
@@ -11,10 +14,16 @@ class TablesManager():
 
 
     def add_table(self, table: InterfaceTable) -> None:
+        """
+        Add table to event loop
+        """
         task = self.__event_loop.create_task(table.pull())
         self.__tables[table.id] = task
 
     def delete_table(self, table_id: str):
+        """
+        Remove table from event loop
+        """
         task = self.__tables.pop(table_id)
         if task:
             task.cancel()
