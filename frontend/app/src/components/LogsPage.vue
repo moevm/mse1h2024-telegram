a73a778839ad10f4a3e286ab854c1dad783dcf37
KirillSamokhin <samohink575@gmail.com>
2024-03-18 22:56:59 +0300

    16: routing and routebar added

---
+++ frontend/app/src/components/LogsPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import RouteBar from "@/components/RouteBar.vue";
+</script>
+
+<template>
+  <v-container style="margin: 0; display: flex; height: 100%">
+    <RouteBar></RouteBar>
+    <h1 style="text-align: center; width: 80%">Здесь будут наши ЛОГИ</h1>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/app/src/components/TablesPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import RouteBar from "@/components/RouteBar.vue";
+</script>
+
+<template>
+  <v-container style="margin: 0">
+    <RouteBar></RouteBar>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
