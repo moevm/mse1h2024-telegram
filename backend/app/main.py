8ffb5d86b7f60d42c98dd65fc2c9e6c9b9b80149
homin12 <vova.chernuxa@mail.ru>
2024-03-18 14:09:33 +0300

    uri fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from logging.config import dictConfig
 import logging
 from .config.log_config import LogConfig
 from database import init_db
+import os 
 
 dictConfig(LogConfig().dict())
 logger = logging.getLogger('MSE-telegram')
@@ -27,8 +28,10 @@ app.include_router(ping.router)
 app.include_router(auth.router)
 app.include_router(add_sample_task.router)
 app.include_router(crud.router)
+user, passwd, db_name, db_host = os.getenv('MONGO_USER'), os.getenv('MONGO_PASS'), os.getenv('MONGO_DB'), os.getenv('MONGO_HOST')
+
 
 @app.on_event('startup')
 async def startup_event():
     logger.info('Server started')
-    await init_db("mongodb://localhost:27017/bot_db", "bot_db")
+    await init_db(f"mongodb://{user}:{passwd}@{db_host}/{db_name}", db_name)
