882c21d04b23e4b2abab572f6b8a64e13d442b66
KirillSamokhin <samohink575@gmail.com>
2024-03-19 11:57:06 +0300

    16: modified

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <router-view/>
+  <RouterView />
 </template>
+
+<style scoped>
+</style>
--- frontend/app/src/components/TablesPage.vue
+++ frontend/app/src/components/TablesPage.vue
@@ -5,19 +5,6 @@ import RouteBar from "@/components/RouteBar.vue";
 <template>
   <v-container style="margin: 0">
     <RouteBar></RouteBar>
-<!--      <v-layout>-->
-<!--        <v-navigation-drawer-->
-<!--            image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"-->
-<!--            theme="dark"-->
-<!--            permanent-->
-<!--        >-->
-<!--          <v-list nav>-->
-<!--            <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>-->
-<!--            <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>-->
-<!--            <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>-->
-<!--          </v-list>-->
-<!--        </v-navigation-drawer>-->
-<!--      </v-layout>-->
   </v-container>
 </template>
 
