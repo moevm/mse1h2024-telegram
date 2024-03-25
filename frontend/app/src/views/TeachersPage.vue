4b661d69014e22c811742329468b95d6e7c7b606
KirillSamokhin <samohink575@gmail.com>
2024-03-25 22:09:53 +0300

    48: добавлен поиск

--- frontend/app/src/views/TeachersPage.vue
+++ frontend/app/src/views/TeachersPage.vue
@@ -2,76 +2,83 @@
 import { ref, computed } from 'vue';
 import type TeacherItem from "@/entities/TeacherEntity";
 import TeacherCreator from "@/models/TeacherModel";
-import TableCreator from "@/models/TableModel";
+import { mdiMagnify } from '@mdi/js';
 
 const teachers = new TeacherCreator();
 
-const itemsPerPage = ref(14);
+const itemsPerPage = ref(12);
 const currentPage = ref(1);
-const totalPages = computed(() => Math.ceil(teachers.data.length / itemsPerPage.value));
+const totalPages = computed(() => Math.ceil(teachersList.value.length / itemsPerPage.value));
+
+const filterList = (searchable: string) => {
+  if(searchable != '') {
+    console.log(teachersData.value.filter((item) => item.aliases.indexOf(searchable) != -1));
+    teachersList.value = teachersData.value.filter((item) => item.aliases.indexOf(searchable) != -1);
+    currentPage.value = 1
+  }
+  else{
+    console.log(teachersData.value)
+    teachersList.value = teachersData.value;
+  }
+}
 
 const teacher1: TeacherItem = {
+  _id: 1,
   username: '@testusername1',
   aliases: 'Иванов Дмитрий Владимирович'
 }
 
 const teacher2: TeacherItem = {
+  _id: 2,
   username: '@testusername2',
   aliases: 'Заславский Марк Маркович'
 }
 
 teachers.addTeacher(teacher1)
 teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher1)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher2)
-teachers.addTeacher(teacher2)
+
+const teachersData = ref(teachers.data)
+const teachersList = ref(teachers.data)
+
+const items = ['Иванов Дмитрий Владимирович', 'Заславский Марк Маркович']
 </script>
 
 <template>
   <v-container style="width: 80%">
     <v-row justify="start">
       <v-col cols="12" md="10" sm="6">
+        <v-autocomplete
+            :items="items"
+            :prepend-inner-icon="mdiMagnify"
+            class="mx-auto"
+            density="comfortable"
+            menu-icon=""
+            placeholder="Поиск преподавателей"
+            style="max-width: 350px;"
+            theme="light"
+            variant="solo"
+            auto-select-first
+            item-props
+            @update:search="filterList"
+        ></v-autocomplete>
         <v-table density="compact">
           <thead>
           <tr>
             <th class="text-center">
-              Username
+              ФИО
             </th>
             <th class="text-center">
-              Aliases
+              Имя пользователя
             </th>
           </tr>
           </thead>
           <tbody>
           <tr
-              v-for="item in teachers.data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)"
+              v-for="item in teachersList.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)"
               :key="item.username"
           >
-            <td class="text-table">{{ item.username }}</td>
             <td class="text-table">{{ item.aliases }}</td>
+            <td class="text-table">{{ item.username }}</td>
           </tr>
           </tbody>
         </v-table>
