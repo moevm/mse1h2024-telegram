267484bd1536978ce4d2c153a19311cd78c783cd
ShlyapaMan <GeorgiOlio@yandex.ru>
2024-03-25 15:40:06 +0300

    create transition

--- frontend/app/src/views/Login.vue
+++ frontend/app/src/views/Login.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import axios from 'axios';
 import TelegramLogin from '@/components/TelegramLogin.vue';
 
-const props = defineProps({
-  botName: String
-});
-
-const botName = import.meta.env.VITE_TELEGRAM_BOT_NAME;
+const router = useRouter();
+const errorMessage = ref('');
+const botName = ref(import.meta.env.VITE_TELEGRAM_BOT_NAME);
 
 function authenticateUser(user: any) {
 	axios.get('http://localhost:8000/auth/login', {
@@ -20,7 +19,11 @@ function authenticateUser(user: any) {
 			photo_url: user.photo_url,
 			auth_date: user.auth_date,
 			hash: user.hash
-		}
+			}
+    }).then((response) => {
+		router.push('/admin');
+    }).catch((error) => {
+		errorMessage.value = `Error: ${error.response.status} ${error.response.statusText}`;
 	});
 }
 </script>
@@ -28,32 +31,40 @@ function authenticateUser(user: any) {
 <template>
 	<div id="centered-content">
 		<h1 id="centered-text">
-				Админ панель<br/>Бота напоминаний
+			Админ панель<br/>Бота напоминаний
 		</h1>
 		<TelegramLogin
-				size="large"
-				:bot-name="botName"
-				requestAccess="write"
-				@callback="authenticateUser"
-				id="telegramAuth" />
+			size="large"
+			:botName="botName"
+			requestAccess="write"
+			@callback="authenticateUser"
+			id="telegramAuth"
+		/>
+		<p id="error">{{ errorMessage }}</p>
 	</div>
 </template>
 
 <style scoped>
 #centered-content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 85vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 85vh;
 }
 #centered-text {
-    margin-bottom: 50px;
-    text-align: center;
-    font-weight: 470;
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	margin-bottom: 50px;
+	text-align: center;
+	font-weight: 470;
+	font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 #telegramAuth {
-    transform: scale(1.5);
+	transform: scale(1.5);
+}
+#error {
+	margin-top: 15px;
+	color: darkred;
+	font-weight: 600;
+	font-size: medium;
 }
 </style>
