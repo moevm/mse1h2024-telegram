619f0e7bc8b1442aee0be8c4fc299d64088da581
homin12 <vova.chernuxa@mail.ru>
2024-03-26 18:13:03 +0300

    bugfix

--- telegram_bot/bot/main.py
+++ telegram_bot/bot/main.py
@@ -23,8 +23,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     logger.info(update.effective_user.id)
     logger.info(update.effective_chat.id)
     user = update.effective_user
-    async with client:
-        await client.post("http://backend:8000/users", json = {'username': f'{user.name}', "chat_id": f'{update.effective_chat.id}'})
+    await client.post("http://backend:8000/users", json = {'username': f'{user.name}', "chat_id": f'{update.effective_chat.id}'})
     logger.info(update.effective_chat.id)
     await update.message.reply_html(rf"Hi, {user.mention_html()}!", reply_markup=ForceReply(selective=True))
 
