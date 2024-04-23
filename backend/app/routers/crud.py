d4c9886fcc4957a2f19f5c7ba574fbff76201a28
homin12 <vova.chernuxa@mail.ru>
2024-04-23 13:52:29 +0300

    add admin

--- backend/app/routers/crud.py
+++ backend/app/routers/crud.py
@@ -3,57 +3,57 @@ from beanie import PydanticObjectId
 from typing import List
 from ..models.db_models import Teacher, Log, Table, TelegramUser, Page, Statistic
 from ..tables.tables_manager import TablesManager
-
+from ..deps import Admin
 router = APIRouter()
 
 
 @router.get("/tables", response_model=List[Table])
-async def get_all_tables():
+async def get_all_tables(admin: Admin):
     return await Table.find_all().to_list()
 
 
 @router.get("/users", response_model=List[TelegramUser])
-async def get_all_users():
+async def get_all_users(admin: Admin):
     return await TelegramUser.find_all().to_list()
 
 
 @router.get("/users/{user_id}", response_model=TelegramUser)
-async def get_user(user_id: PydanticObjectId):
+async def get_user(user_id: PydanticObjectId, admin: Admin):
     return await TelegramUser.get(user_id)
 
 
 @router.get("/tables/{table_id}", response_model=Table)
-async def get_table(table_id: PydanticObjectId):
+async def get_table(table_id: PydanticObjectId, admin: Admin):
     return await Table.get(table_id)
 
 
 @router.get("/logs", response_model=List[Log])
-async def get_all_logs():
+async def get_all_logs(admin: Admin):
     return await Log.find_all().to_list()
 
 
 @router.get("/logs/{log_id}", response_model=Log)
-async def get_log(log_id: PydanticObjectId):
+async def get_log(log_id: PydanticObjectId, admin: Admin):
     return await Log.get(log_id)
 
 
 @router.get("/teachers", response_model=List[Teacher])
-async def get_all_teachers():
+async def get_all_teachers(admin: Admin):
     return await Teacher.find_all().to_list()
 
 
 @router.get("/statistics", response_model=List[Statistic])
-async def get_all_statistics():
+async def get_all_statistics(admin: Admin):
     return await Statistic.find_all().to_list()
 
 
 @router.get("/teachers/{teacher_id}", response_model=Teacher)
-async def get_teacher(teacher_id: PydanticObjectId):
+async def get_teacher(teacher_id: PydanticObjectId, admin: Admin):
     return await Teacher.get(teacher_id)
 
 
 @router.post("/tables", response_model=Table)
-async def create_table(table: Table):
+async def create_table(table: Table, admin: Admin):
     await table.create()
     TablesManager().add_table(table)
     return table
@@ -68,7 +68,7 @@ async def create_user(user: TelegramUser):
 
 
 @router.post("/tables/{table_id}", response_model=Table)
-async def create_rule(table_id: PydanticObjectId, page: Page):
+async def create_rule(table_id: PydanticObjectId, page: Page, admin: Admin):
     table = await Table.get(table_id)
     if not table:
         raise HTTPException(
@@ -88,19 +88,19 @@ async def create_rule(table_id: PydanticObjectId, page: Page):
 
 
 @router.post("/teachers", response_model=Teacher)
-async def add_teacher(teacher: Teacher):
+async def add_teacher(teacher: Teacher, admin: Admin):
     await teacher.create()
     return teacher
 
 
 @router.post("/statistics", response_model=Statistic)
-async def add_statistic(statistic: Statistic):
+async def add_statistic(statistic: Statistic, admin: Admin):
     await statistic.insert()
     return statistic
 
 
 @router.delete("/tables/{table_id}", response_model=Table)
-async def delete_table(table_id: PydanticObjectId):
+async def delete_table(table_id: PydanticObjectId, admin: Admin):
     table = await Table.get(table_id)
     if not table:
         raise HTTPException(
@@ -113,7 +113,7 @@ async def delete_table(table_id: PydanticObjectId):
 
 
 @router.delete("/users/{username}", response_model=TelegramUser)
-async def delete_user(username: str):
+async def delete_user(username: str, admin: Admin):
     query = await TelegramUser.find(TelegramUser.username == username).to_list()
     if not query:
         raise HTTPException(
@@ -126,7 +126,7 @@ async def delete_user(username: str):
 
 
 @router.delete("/teachers/{teacher_id}", response_model=Teacher)
-async def delete_teacher(teacher_id: PydanticObjectId):
+async def delete_teacher(teacher_id: PydanticObjectId, admin: Admin):
     teacher = await Teacher.get(teacher_id)
     if not teacher:
         raise HTTPException(
@@ -138,7 +138,7 @@ async def delete_teacher(teacher_id: PydanticObjectId):
 
 
 @router.delete("/tables/{table_id}/{page_id}", response_model=Table)
-async def delete_table_rule(table_id: PydanticObjectId, page_id: str):  # p_id - uuid of page
+async def delete_table_rule(table_id: PydanticObjectId, page_id: str, admin: Admin):  # p_id - uuid of page
     table = await Table.get(table_id)
     if not table:
         raise HTTPException(
@@ -156,7 +156,7 @@ async def delete_table_rule(table_id: PydanticObjectId, page_id: str):  # p_id -
 
 
 @router.put("/tables/{table_id}", response_model=Table)
-async def edit_table(table_id: PydanticObjectId, req_table: Table):
+async def edit_table(table_id: PydanticObjectId, req_table: Table, admin: Admin):
     table = await Table.get(table_id)
     if not table:
         raise HTTPException(
@@ -170,7 +170,7 @@ async def edit_table(table_id: PydanticObjectId, req_table: Table):
 
 
 @router.put("/users/{user_id}", response_model=TelegramUser)
-async def edit_user(user_id: PydanticObjectId, req_user: TelegramUser):
+async def edit_user(user_id: PydanticObjectId, req_user: TelegramUser, admin: Admin):
     user = await TelegramUser.get(user_id)
     if not user:
         raise HTTPException(
@@ -182,7 +182,7 @@ async def edit_user(user_id: PydanticObjectId, req_user: TelegramUser):
 
 
 @router.put("/table/{table_id}/{page_id}", response_model=Table)
-async def edit_rule(table_id: PydanticObjectId, page_id: str, req_page: Page):
+async def edit_rule(table_id: PydanticObjectId, page_id: str, req_page: Page, admin: Admin):
     table = await Table.get(table_id)
     if not table:
         raise HTTPException(
@@ -205,7 +205,7 @@ async def edit_rule(table_id: PydanticObjectId, page_id: str, req_page: Page):
 
 
 @router.put("/teachers/{teacher_id}", response_model=Teacher)
-async def edit_teacher(teacher_id: PydanticObjectId, req_teacher: Teacher):
+async def edit_teacher(teacher_id: PydanticObjectId, req_teacher: Teacher, admin: Admin):
     teacher = await Teacher.get(teacher_id)
     if not teacher:
         raise HTTPException(
