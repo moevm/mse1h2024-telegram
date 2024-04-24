0fa03b192bbf69a2d75f70684672e1368aee18e0
d1mitrii <dyusmirnov@stud.etu.ru>
2024-04-24 16:13:59 +0300

    fix: now chrome without gpu

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from base_selenium import BaseSelenium
 
 link = "http://localhost:8080/"
 
@@ -14,74 +15,68 @@ def password_testcases():
     ]
 
 
-class TestAuth():
-
-    def setup_method(self):
-        self.browser = webdriver.Chrome()
-
-    def teardown_method(self):
-        self.browser.quit()
+class TestAuth(BaseSelenium):
 
     def test_should_see_login_page(self):
-        self.browser.get(link)
-        self.browser.find_element(By.ID, "sign-in-button")
+        self.driver.get(link)
+        self.driver.find_element(By.ID, "sign-in-button")
 
     def test_login_dialog(self):
-        self.browser.get(link)
-        login_button = self.browser.find_element(By.ID, "sign-in-button")
+        self.driver.get(link)
+        login_button = self.driver.find_element(By.ID, "sign-in-button")
 
         assert login_button.is_enabled(), "Кнопка входа не активна"
 
         login_button.click()
-        WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(
             (By.ID, "sign-in-dialog")
         ))
 
     def test_correct_password(self):
-        self.browser.get(link)
-        login_button = self.browser.find_element(By.ID, "sign-in-button")
+        self.driver.get(link)
+        login_button = self.driver.find_element(By.ID, "sign-in-button")
 
         assert login_button.is_enabled(), "Кнопка входа не активна"
 
         login_button.click()
-        WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(
             (By.ID, "sign-in-dialog")
         ))
-        password_input = self.browser.find_element(By.ID, "password-field")
+        password_input = self.driver.find_element(By.ID, "password-field")
         password_input.send_keys("1234")
 
-        confirm = self.browser.find_element(By.ID, "confirm-button")
+        confirm = self.driver.find_element(By.ID, "confirm-button")
         confirm.click()
-        WebDriverWait(self.browser, 10).until(
-            EC.url_changes(self.browser.current_url)
+        WebDriverWait(self.driver, 10).until(
+            EC.url_changes(self.driver.current_url)
         )
 
-        assert self.browser.current_url == (link+"admin")
+        assert self.driver.current_url == (link+"admin")
 
     def test_incorrect_passwords(self, password_testcases):
-        self.browser.get(link)
-        login_button = self.browser.find_element(By.ID, "sign-in-button")
+        self.driver.get(link)
+        login_button = self.driver.find_element(By.ID, "sign-in-button")
 
         assert login_button.is_enabled(), "Кнопка входа не активна"
 
         login_button.click()
-        WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(
             (By.ID, "sign-in-dialog")
         ))
-        password_input = self.browser.find_element(By.ID, "password-field")
+        password_input = self.driver.find_element(By.ID, "password-field")
         for testcase in password_testcases:
             password_input.clear()
             password_input.send_keys(testcase[0])
 
-            confirm = self.browser.find_element(By.ID, "confirm-button")
+            confirm = self.driver.find_element(By.ID, "confirm-button")
             confirm.click()
 
-            WebDriverWait(self.browser, 5).until(
+            WebDriverWait(self.driver, 5).until(
                 EC.presence_of_element_located(
                     (By.ID, "password-field-messages")
                 )
             )
-            message = self.browser.find_element(
+            message = self.driver.find_element(
                 By.ID,
                 "password-field-messages"
             )
