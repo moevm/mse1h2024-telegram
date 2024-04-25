b05604b3ebc9076be54804224788c72f57e464e3
Ignashov Vadim Maksimovich <48078801+ImSOLty@users.noreply.github.com>
2024-04-25 13:42:49 +0300

    Add authentication checker

--- tests/selenium_tests/test_admin.py
+++ tests/selenium_tests/test_admin.py
@@ -1,23 +1,30 @@
 import pytest
+from selenium.common import JavascriptException
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 from .pages.auth_page import AuthPage
 from .pages.admin_main_page import AdminMainPage
+from .utils.js_scripts import JSScripts
 from .utils.routes import Routes
 from .utils.constants import DataForTests
 
 
 @pytest.fixture(scope='module')
 def auth_driver(driver):
-    auth_page = AuthPage(driver, Routes.AUTH_URL)
-    auth_page.authorize(DataForTests.CORRECT_PASSWORD)
+    # check if authentication is needed
+    try:
+        driver.execute_script(JSScripts.GET_LOCAL_STORAGE_ITEM, 'token')
+        AdminMainPage(driver, Routes.ADMIN_MAIN_URL)
+    except JavascriptException:
+        auth_page = AuthPage(driver, Routes.AUTH_URL)
+        auth_page.authorize(DataForTests.CORRECT_PASSWORD)
     yield driver
 
 
 @pytest.mark.order(2)
 @pytest.mark.selenium_tests
 class TestAdmin:
-
     def test_should_see_admin_page_after_auth(self, auth_driver: WebDriver):
         admin_main_page = AdminMainPage(auth_driver, Routes.ADMIN_MAIN_URL)
         admin_main_page.check_page(Routes.ADMIN_MAIN_URL)
+    
---
+++ tests/selenium_tests/utils/js_scripts.py
@@ -0,0 +1,2 @@
+class JSScripts:
+    GET_LOCAL_STORAGE_ITEM = "return window.localStorage.getItem(arguments[0]);"
